65195fe26808fef4583627a315ddd3758126a509
Alishan Ladhani <alishan@retailcommon.com>
2018-04-30 13:26:44 -0400

    Remove crowdsale contracts

--- migrations/3_deploy_crowdsale.js
+++
@@ -1,8 +0,0 @@
-const LambdaTokenSale = artifacts.require('./LambdaTokenSale.sol');
-const LambdaToken = artifacts.require('./LambdaToken.sol');
-
-module.exports = function(deployer, network, accounts) {
-  const rate = new web3.BigNumber(1);
-  const wallet = accounts[0];
-  deployer.deploy(LambdaTokenSale, rate, wallet, LambdaToken.address);
-};
